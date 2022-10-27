edef7173ac99b10b3cb663f0b54a7f746017a20b
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2022-10-27 17:14:05 +0100

    1.0

--- form.js
+++ form.js
@@ -1,19 +1,54 @@
-const fName = document.getElementById("fname");
-const lName = document.getElementById('lname');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+//INPUT SELECTORS
+const fName = document.querySelector(".fname");
+const lName = document.querySelector('.lname');
+const email = document.querySelector('.email');
+const password = document.querySelector('#password');
 const form = document.getElementById('form');
-const submit = document.getElementById("submit");
-const fNameError = docume.getElementById("fNameError");
+//ERRORS SELECTORS
+const fNameError = document.querySelector(".fNameError");
+const lNameError = document.querySelector(".lNameError");
+const emailError = document.querySelector(".emailError");
+const passwordError = document.querySelector(".passwordError");
 
 form.addEventListener('submit', (e) => {
-    let messageArr = [];
+    let nameArr = [];
     if(fName.value === '' || fName.value == null){
-        messageArr.push(1);
+        nameArr.push(1);
     }
-    if(messageArr.length > 0){
+    if(nameArr.length > 0){
         e.preventDefault();
-        
+        fNameError.classList.toggle("hidden");
+        fName.style.borderColor = "red";
     }
-})
 
+    let lnameArr=[];
+    if(lName.value === '' || lName.value == null){
+        lnameArr.push(1);
+    }
+    if(lnameArr.length > 0){
+        e.preventDefault();
+        lNameError.classList.toggle("hidden");
+        lName.style.borderColor = "red";
+    }
+
+    let emailArr = [];
+    if(email.value === '' || email.value == null){
+        emailArr.push(1);
+    }
+    if(emailArr.length > 0){
+        e.preventDefault();
+        emailError.classList.toggle("hidden");
+        email.style.borderColor = "red";
+    }
+
+    let passwArr = [];
+    if(password.value === '' || password.value == null){
+        passwArr.push(1);
+    }
+    if (passwArr.length > 0){
+        e.preventDefault();
+        passwordError.classList.toggle("hidden");
+        password.style.borderColor = "red";
+    }
+
+})
